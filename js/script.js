aa40fb4f544673a3071142808e5e7cbdf344c486
Marsel <m.obutov@yandex.ru>
2025-11-24 01:44:57 +0300

    fux bug

--- js/script.js
+++ js/script.js
@@ -1,78 +1,75 @@
-const skils = document.querySelector("dl.skill-list");
+const skills = {
+    elem: null,
+    data: [ 
+        {name:"c++", level:50, image:"c++.svg"},
+        {name:"c", level:30,image:"c.svg"},
+        {name:"html", level:20, image:"html.svg"},
+        {name:"css", level:20, image:"css.svg"} 
+    ],
+    sortMode: null,
+    init(text) { this.elem = document.querySelector(text); },
+    generateList() {
+        this.elem.innerHTML = "";
+        this.data.forEach((item) => {
+            const dt = document.createElement("dt");
+            dt.style.backgroundImage = "url(img/"+item.image+")";
+            dt.className = "skill-item";
+            dt.textContent = item.name;
+            this.elem.append(dt);
 
-skils.data = [
-    {name:"c++", level:50, image:"c++.svg"},
-    {name:"c", level:30,image:"c.svg"},
-    {name:"html", level:20, image:"html.svg"},
-    {name:"css", level:20, image:"css.svg"}
-];
+            const div = document.createElement("div");
+            div.style.width = item.level + "%";
+            div.textContent = item.level + "%";
 
-skils.sortMode = null;
+            const dd = document.createElement("dd");
+            dd.className = "skill-level";
+            dd.append(div);
+            this.elem.append(dd);
+        })
+    },
+    getComparer(prop) {
+        return function (a, b) {
+            if (a[prop] < b[prop]) {
+                return -1;
+            }
+            
+            if (a[prop] > b[prop]) {
+                return 1;
+            }
 
-skils.generateList = function(elem) {
-    elem.innerHTML = "";
-    elem.data.forEach((item) => {
-        const dt = document.createElement("dt");
-        dt.style.backgroundImage = "url(img/"+item.image+")";
-        dt.className = "skill-item";
-        dt.textContent = item.name;
-        elem.append(dt);
+            return 0;
+        }
+    },
+    sortList(object_sort) {
+        if(this.sortMode === object_sort) {
+            this.data.reverse();
+        }
+        else {
+            this.data.sort(this.getComparer(object_sort));
+            this.sortMode = object_sort;
+        }
+    }
 
-        const div = document.createElement("div");
-        div.style.width = item.level + "%";
-        div.textContent = item.level + "%";
 
-        const dd = document.createElement("dd");
-        dd.className = "skill-level";
-        dd.append(div);
-        elem.append(dd);
-    })
 }
 
-skils.generateList(skils);
-
-//---------------------------------------
-
-function getComparer(prop) {
-    return function (a, b) {
-        if (a[prop] < b[prop]) {
-            return -1;
-        }
-        
-        if (a[prop] > b[prop]) {
-            return 1;
-        }
-
-        return 0;
-    }
-};
-
-//---------------------------------------
-
-skils.sortList = function (object_sort) {
-    if(this.sortMode === object_sort) {
-        this.data.reverse();
-    }
-    else {
-        this.data.sort(getComparer(object_sort));
-        this.sortMode = object_sort;
-    }
-};
+skills.init("dl.skill-list");
+skills.generateList();
 
 const skills_sort = document.querySelector("div.skills-sort");
 
 skills_sort.addEventListener('click', (e) => {
     if (e.target.nodeName === 'BUTTON') {
-        const _type = e.target.dataset.type;
-        switch (_type) {
+        const type = e.target.dataset.type;
+        switch (type) {
             case 'name':
-                skils.sortList(_type);
-                skils.generateList(skils);
+                skills.sortList(type);
+                skills.generateList(skills);
             break;
 
             case 'level':
-                skils.sortList(_type);
-                skils.generateList(skils);
+                skills.sortList(type);
+                skills.generateList(skills);
             break;
 
             default:
